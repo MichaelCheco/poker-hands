c96075054cad6e433c4e6971eef103d20cf71cac
Michael Checo <checomichael2@gmail.com>
2025-05-18 17:34:37 -0700

    remove blinds from action list

--- api/hands.ts
+++ api/hands.ts
@@ -117,32 +117,32 @@ export async function saveHandToSupabase(
 
     // 3. Insert into 'actions' table
     if (handHistoryData.playerActions && handHistoryData.playerActions.length > 0) {
-        const blinds = [
-            {
-                hand_id: handId,
-                action_index: 0,
-                stage: 0,
-                position: 'SB',
-                decision: 'B',
-                action_amount: setupInfo.smallBlind,
-                // player_stack_before: action.playerStackBefore,
-                pot_size_before: 0,
-                text_description: `SB posts $${setupInfo.smallBlind}`,
-                was_auto_folded: true,
-            },
-            {
-                hand_id: handId,
-                action_index: 1,
-                stage: 0,
-                position: 'BB',
-                decision: 'R',
-                action_amount: setupInfo.bigBlind,
-                // player_stack_before: action.playerStackBefore,
-                pot_size_before: setupInfo.smallBlind,
-                text_description: `BB posts $${setupInfo.bigBlind}`,
-                was_auto_folded: true,
-            },
-        ]
+        // const blinds = [
+        //     {
+        //         hand_id: handId,
+        //         action_index: 0,
+        //         stage: 0,
+        //         position: 'SB',
+        //         decision: 'B',
+        //         action_amount: setupInfo.smallBlind,
+        //         // player_stack_before: action.playerStackBefore,
+        //         pot_size_before: 0,
+        //         text_description: `SB posts $${setupInfo.smallBlind}`,
+        //         was_auto_folded: true,
+        //     },
+        //     {
+        //         hand_id: handId,
+        //         action_index: 1,
+        //         stage: 0,
+        //         position: 'BB',
+        //         decision: 'R',
+        //         action_amount: setupInfo.bigBlind,
+        //         // player_stack_before: action.playerStackBefore,
+        //         pot_size_before: setupInfo.smallBlind,
+        //         text_description: `BB posts $${setupInfo.bigBlind}`,
+        //         was_auto_folded: true,
+        //     },
+        // ]
         const actionsToInsert = handHistoryData.playerActions.map((action, index) => ({
             hand_id: handId,
             action_index: index + 2,
@@ -158,7 +158,7 @@ export async function saveHandToSupabase(
 
         const { error: actionsError } = await supabase
             .from('actions')
-            .insert([...blinds, ...actionsToInsert]);
+            .insert(actionsToInsert);
 
         if (actionsError) {
             console.error("Supabase actions insert error:", actionsError);
